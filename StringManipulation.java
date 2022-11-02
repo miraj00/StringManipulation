d2db77b5f9e90813de3958ac3050637f0082c719
miraj <miraj00@gmail.com>
2022-11-01 23:38:59 -0400

    requirements added in comments

--- StringManipulation.java
+++ StringManipulation.java
@@ -132,4 +132,46 @@ public class StringManipulation {
 
 
 
+/*  ================================== Requirements ====================================================
+
+ Strings & Methods
+
+Strings, Methods, Parameters
+
+Task: Create a class named StringLab. Add the standard main method.
+
+Add the following public static void methods, and call each one at least once from your main method to test them.
+
+capitalize(String word) - takes a word and prints it out with the first letter capitalized and all other letters lowercase.
+E.g. for “blAZes”, it prints “Blazes”.
+
+wheresWaldo(String phrase) - takes a string and prints the location (index) in the string where the word “Waldo” first appears.
+E.g. for “Striped Waldo!”, it prints “8”.
+
+firstThingsFirst(String a, String b) - takes two strings, prints the string that comes first alphabetically, then prints the other.
+E.g. for “cat” and “dog”, it prints: “cat dog”; for “tree” and “pine”, it prints: “pine tree”.
+
+reverse(String s) - takes a string and prints it backwards
+E.g. for “happy”, it prints “yppah”.
+
+soLong(String a, String b) - prints only the longer of the two strings. If both strings are the same length, print both.
+E.g. for “bean” and “pea”, it prints “bean”; for “tart” and “trap”, it prints “tart trap”
+
+afterMath(String phrase) - takes a phrase. If the phrase contains the word math, it prints the rest of the phrase starting with math. If not, it prints “dud”.
+E.g. for “She aced the mathematics test!”, it prints “mathematics test!”; for “My floor mat is so keen!”, it prints “dud”.
+
+letterize(String word) - takes a word. Prints it out one letter per line. Hint: Use a loop.
+E.g. for “Java”, it prints
+J
+a
+v
+a
+
+Extended Challenges
+For capitalize, wheresWaldo, firstThingsFirst, reverse, and afterMath return the result rather than printing it. Then print it in the main method instead.
+Put all these methods in a separate StringMethods class and still call them from your main method in StringLab.
+Create a camelCase method. It takes in a phrase and turns it into a camel case variable name.
+E.g. for “desert survival skills”, it prints “desertSurvivalSkills”.
+
+ */
 
